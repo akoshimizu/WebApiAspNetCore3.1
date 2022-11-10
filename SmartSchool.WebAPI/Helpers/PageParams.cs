30b63077cfe60de60c38120463f0e504ae425125
akoshimizu <anderson_koshimizu@hotmail.com>
2022-11-10 10:03:55 -0300

    Inserção - Filtragem

--- SmartSchool.WebAPI/Helpers/PageParams.cs
+++ SmartSchool.WebAPI/Helpers/PageParams.cs
@@ -10,5 +10,9 @@ namespace SmartSchool.WebAPI.Helpers
             set{ _pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
 
         }
+
+        public int? Matricula { get; set; } = null;
+        public string Nome { get; set; } = string.Empty;
+        public int? Ativo { get; set; } = null;
     }
 }
